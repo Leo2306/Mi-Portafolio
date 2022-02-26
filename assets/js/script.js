208b47177baae26bf25c908e5884dd3419d40b33
Leo2306 <leonardonarvaja232@gmail.com>
2022-02-26 19:02:59 -0300

    Modificando nombres de clases y añadiendo estilos al icono del menu desplegable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,18 @@
 const menuNavegacion = document.getElementById("navegacion-menu"),
       toggleNavegacion = document.getElementById("navegacion-toggle"),
       menuClose = document.getElementById("navegacion-close"),
-      contacto = document.getElementById("contacto")
+      contacto = document.getElementById("navegacion-contacto");
 
 if(toggleNavegacion) {
     toggleNavegacion.addEventListener("click", function() {
         menuNavegacion.classList.add("show");
-        contacto.classList.add("show-contact");
+        contacto.classList.add("show");
     })
 }
 
 if(menuClose) {
     menuClose.addEventListener("click", function() {
     menuNavegacion.classList.remove("show");
-    contacto.classList.remove("show-contact");
+    contacto.classList.remove("show");
     })
 }
