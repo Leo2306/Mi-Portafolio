f5f246d70a216b9890e700372558d48c8542079b
Leo2306 <leonardonarvaja232@gmail.com>
2022-02-26 18:33:13 -0300

    Añadiendo responsive a la navegacion con menu desplegable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,18 @@
 const menuNavegacion = document.getElementById("navegacion-menu"),
-      toggleNavegacion = document.getElementById("navegacion-toggle");
-      menuClose = document.getElementById("navegacion-close");
+      toggleNavegacion = document.getElementById("navegacion-toggle"),
+      menuClose = document.getElementById("navegacion-close"),
+      contacto = document.getElementById("contacto")
 
 if(toggleNavegacion) {
     toggleNavegacion.addEventListener("click", function() {
         menuNavegacion.classList.add("show");
+        contacto.classList.add("show-contact");
     })
 }
 
 if(menuClose) {
-    toggleNavegacion.addEventListener("click", function() {
+    menuClose.addEventListener("click", function() {
     menuNavegacion.classList.remove("show");
+    contacto.classList.remove("show-contact");
     })
 }
