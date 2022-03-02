2c73802ec64865d1b9203058931b971522e35616
Leonardo <leonardonarvaja232@gmail.com>
2022-03-02 14:25:29 -0300

    Estilos al header y resposive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,21 @@
 const menuNavegacion = document.getElementById("navegacion-menu"),
       toggleNavegacion = document.getElementById("navegacion-toggle"),
-      menuClose = document.getElementById("navegacion-close"),
-      contacto = document.getElementById("navegacion-contacto");
+      menuClose = document.getElementById("navegacion-close");
 
 if(toggleNavegacion) {
     toggleNavegacion.addEventListener("click", function() {
         menuNavegacion.classList.add("show");
         menuClose.classList.add("show-close");
-        contacto.classList.add("show");
         toggleNavegacion.setAttribute("style","z-index: 0");
+        document.body.classList.add("blur");
+        menuNavegacion.classList.add("no-blur");
     })
 }
 
 if(menuClose) {
     menuClose.addEventListener("click", function() {
     menuNavegacion.classList.remove("show");
-    menuClose.classList.remove("show-close")
-    contacto.classList.remove("show");
+    menuClose.classList.remove("show-close");
+    document.body.classList.remove("blur");
     })
 }
