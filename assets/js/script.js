e53487952bb6e3d8c398b5f08e26cccad3bdc8a3
Leo2306 <leonardonarvaja232@gmail.com>
2022-03-02 18:26:12 -0300

    Reorganizando el codigo y finalizando las modificaciones en el header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 const menuNavegacion = document.getElementById("navegacion-menu"),
       toggleNavegacion = document.getElementById("navegacion-toggle"),
-      menuClose = document.getElementById("navegacion-close");
+      menuClose = document.getElementById("navegacion-close"),
+      main = document.getElementById("main");
 
 if(toggleNavegacion) {
     toggleNavegacion.addEventListener("click", function() {
         menuNavegacion.classList.add("show");
         menuClose.classList.add("show-close");
         toggleNavegacion.setAttribute("style","z-index: 0");
-        document.body.classList.add("blur");
-        menuNavegacion.classList.add("no-blur");
+        main.classList.add("blur");
     })
 }
 
@@ -16,6 +16,6 @@ if(menuClose) {
     menuClose.addEventListener("click", function() {
     menuNavegacion.classList.remove("show");
     menuClose.classList.remove("show-close");
-    document.body.classList.remove("blur");
+    main.classList.remove("blur");
     })
 }
