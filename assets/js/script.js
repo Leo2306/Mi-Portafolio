3e32498bc84512e6c0bd8521378758b2057c8130
Leonardo <leonardonarvaja232@gmail.com>
2022-03-19 21:52:38 -0300

    Añadiendo validaciones al formulario y modificando estilos al sitio

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,31 +20,7 @@ if(menuClose) {
     })
 }
 
-const proyecto = document.querySelector(".proyectos__descripcion"),
-      elemento = document.querySelectorAll(".proyectos__item");
-
-for (let i = 0; i < elemento.length; i++) {
-    
-    elemento[i].addEventListener("click",function(){
-
-        let posicion = i ;
-        let operacion = i * (-100/3);
-
-        proyecto.setAttribute(`style`,`transform: translateX(${operacion}%)`);
-
-        for (let i = 0; i < elemento.length; i++) {
-            
-            elemento[i].classList.remove("activo");
-            
-        } 
-
-        elemento[i].classList.add("activo");
-
-    })
-    
-}
-
-const inputs = document.querySelectorAll(".input");
+const inputs = document.querySelectorAll(".formulario__input");
 
 function focusFunc() {
   let parent = this.parentNode;
@@ -65,9 +41,109 @@ inputs.forEach((input) => {
 
 const enlaces = document.querySelectorAll(".navegacion__enlace");
 
-enlaces.forEach(enlace  => {
+enlaces.forEach(enlace => {
   enlace.addEventListener("click", () => {
     menuNavegacion.classList.remove("show");
     content.classList.remove("blur");
   })
 });
+
+const formulario = document.querySelector(".contacto__formulario")
+
+const validaciones = {
+  nombre: /^[a-zA-ZÁ-ÿ\s]+$/,
+  apellido: /^[a-zA-ZÁ-ÿ\s]+$/,
+  email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z\s0-9-]+\.[a-zA-Z\s0-9-.]+/,
+  mensaje: /(.)/
+}
+
+const campos = {
+  nombre: false,
+  apellido: false,
+  email:false,
+  mensaje: false
+}
+
+const validarFormulario = (event) => {
+
+  switch (event.target.name) {
+
+    case "nombre":
+      validarCampos(validaciones.nombre, event.target, 'nombre');
+    break;
+
+    case "apellido":
+      validarCampos(validaciones.apellido, event.target, 'apellido');
+    break;
+
+    case "email":
+      validarCampos(validaciones.email, event.target, 'email');
+    break;
+    
+    case "mensaje":
+      validarCampos(validaciones.mensaje, event.target, 'mensaje');
+    break;
+
+  }
+}
+
+inputs.forEach(input => {
+  input.addEventListener("keyup", validarFormulario);
+})
+
+const validarCampos = (expresion, input, campo) => {
+  if(input.value == "") {
+
+    document.querySelector(`.campo-${campo} .formulario__estado-${campo}`).classList.remove("fa-circle-xmark");
+    document.querySelector(`.campo-${campo} .formulario__estado-${campo}`).classList.remove("fa-circle-check");
+    document.querySelector(`.formulario__${campo}`).classList.remove("activo");
+
+  } else {
+
+    if(expresion.test(input.value)) {
+      document.querySelector(`.campo-${campo}`).classList.remove("formulario__campo--incorrecto");
+      document.querySelector(`.campo-${campo}`).classList.add("formulario__campo--correcto");
+      document.querySelector(`.campo-${campo} .formulario__estado-${campo}`).classList.remove("fa-circle-xmark");
+      document.querySelector(`.campo-${campo} .formulario__estado-${campo}`).classList.add("fa-circle-check");
+      document.querySelector(`.formulario__${campo}`).classList.remove("activo");
+      campos[campo] = true;
+    } else {
+      document.querySelector(`.campo-${campo}`).classList.remove("formulario__campo--correcto");
+      document.querySelector(`.campo-${campo}`).classList.add("formulario__campo--incorrecto");
+      document.querySelector(`.campo-${campo} .formulario__estado-${campo}`).classList.remove("fa-circle-check");
+      document.querySelector(`.campo-${campo} .formulario__estado-${campo}`).classList.add("fa-circle-xmark");
+      document.querySelector(`.formulario__${campo}`).classList.add("activo");
+      campos[campo] = false;
+    }
+  }
+}
+
+formulario.addEventListener("submit", (event) => {
+
+  event.preventDefault();
+
+  if(campos.nombre && campos.apellido && campos.email && campos.mensaje) {
+    document.querySelector(".formulario__estado-valido").classList.add("activo");
+    setTimeout(() => {document.querySelector(".formulario__estado-valido").classList.remove("activo")},2500);
+    
+    inputs.forEach(input => {
+      input.parentNode.classList.remove("focus");
+      input.parentNode.classList.remove("formulario__campo--correcto");
+      campos[input.name] = false;
+    })
+
+    formulario.reset();
+
+  } else {
+    inputs.forEach(input => {
+      if(input.value == "") {
+        document.querySelector(`.formulario__vacio-${input.name}`).classList.add("activo");
+        setTimeout(() => {document.querySelector(`.formulario__vacio-${input.name}`).classList.remove("activo")},2500);
+      } else {
+        document.querySelector(`.formulario__vacio-${input.name}`).classList.remove("activo");
+      }
+    })
+    document.querySelector(".formulario__estado-invalido").classList.add("activo");
+    setTimeout(() => {document.querySelector(".formulario__estado-invalido").classList.remove("activo")},2500);
+  }
+})
